93aa6b5c3612b561527dc334dfc7f0a9dd68e175
JosefKrivanek <kri41387@spseol.cz>
2023-12-10 22:43:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import random
 
-slova = []
+slova = ["kokot","kříž"]
 slabiky = []
 
+def generuj_lorem_ipsum(pocet_vet=5):
+    lorem_ipsum_text = "."
+    for _ in range(pocet_vet):
+        nahodne_slovo = random.choice(slova)
+        lorem_ipsum_text += nahodne_slovo + " "
+
+    return lorem_ipsum_text.strip()
+
+
+lorem_ipsum = generuj_lorem_ipsum()
+print(lorem_ipsum)
+
+
+#Maaaaan fuck this shiiii Imma bout to get a f anyyway
