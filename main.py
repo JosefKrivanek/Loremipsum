10b52c812a4b05829239d6607039c24f454dc2f1
JosefKrivanek <kri41387@spseol.cz>
2023-12-13 02:09:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import random
 
-slova = ["kokot","kříž"]
-slabiky = []
-
-def generuj_lorem_ipsum(pocet_vet=5):
-    lorem_ipsum_text = "."
-    for _ in range(pocet_vet):
-        nahodne_slovo = random.choice(slova)
-        lorem_ipsum_text += nahodne_slovo + " "
-
-    return lorem_ipsum_text.strip()
+slova = ["skakal","pes","přes","oves","zelenou","louku","šel","za","ním","myslivec","péro","na","klobouku",]
 
 
-lorem_ipsum = generuj_lorem_ipsum()
-print(lorem_ipsum)
+pocet_odstavcu = int(input("Zadejte požadovaný počet odstavců: "))
+for i in range(pocet_odstavcu):
+    for i in range(random.randint(5,10)):
+        veta = ""
+        for i in range(random.randint(8,30)):
+            nahodne_slovo = random.choice(slova)
+            veta += nahodne_slovo + " "
+        VETA = veta.capitalize()
+        print(VETA + ".")
+    print("\n")
 
+
+
+#def generuj_lorem_ipsum(slova=50):
+#    lorem_ipsum_text = ""
+#    for _ in range(delka):
+#        nahodna_veta = random.choice(slova)
+#        lorem_ipsum_text += nahodna_veta + " "
+#    return lorem_ipsum_text
+
+#lorem_ipsum = generuj_lorem_ipsum()
+#print(lorem_ipsum)
+
+
+#def text (dleka):
+#    for i in range(random.randint(5, 15)):
+#        nahodne_slovo = random.choice(slova)
+#        veta += nahodne_slovo + " "
+#    return text
+#lorem_ipsum = text()
+#print(lorem_ipsum)
 
 #Maaaaan fuck this shiiii Imma bout to get a f anyyway
